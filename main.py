4f3f201f7cc8d6538db5496e0dba935a626ed690
James <51536154+mbruhyo@users.noreply.github.com>
2021-02-01 18:37:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = Quart(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 
 dir = '/home/iteki/gulag/.data/avatars'
+rvn = '/home/iteki/gulag/.data/osr'
+rrx = '/home/iteki/gulag/.data/osr_rx'
+rap = '/home/iteki/gulag/.data/osr_ap'
 
 @app.route("/<int:id>")
 async def avatar(id):
@@ -14,6 +17,26 @@ async def avatar(id):
         file = "default"
     
     return await send_file(f"{dir}/{file}.png")
+
+@app.route("/replay/<int:id>")
+async def dl_replay(id):
+    if os.path.isfile(f"{rvn}/{id}.osr"):
+        file = f'{rvn}/{id}.osr'
+        return await send_file(file, as_attachment=True, attachment_filename=f'{id}.osr')
+    else:
+        e = os.path.isfile(f"{rrx}/{id}.osr")
+    
+    if e:
+        file = f'{rrx}/{id}.osr'
+        return await send_file(file, as_attachment=True, attachment_filename=f'{id}.osr')
+    else:
+        e = os.path.isfile(f"{rap}/{id}.osr")
+    
+    if e:
+        file = f'{rap}/{id}.osr'
+        return await send_file(file, as_attachment=True, attachment_filename=f'{id}.osr')
+    else:
+        return 'Invalid replay ID!'
 
 if __name__ == '__main__':
     app.run()
