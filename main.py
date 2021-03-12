307552619d5c05c64aa1c4394ace50f100a4b5ef
James <51536154+tsunyoku@users.noreply.github.com>
2021-03-12 11:55:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,16 @@ import hashlib
 import data as dataTypes
 
 app = Quart(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
-
-DIR = '/home/iteki/gulag/.data/avatars'
 db = AsyncSQLPool()
 
-async def build_replay(sid: int, mode: str):
+async def build_replay(sid, mode):
     await db.connect(glob.config.mysql)
-    score = await db.fetch(
-        f"SELECT scores_{mode}.*, users.name FROM scores_{mode}"
-        f" LEFT JOIN users ON scores_{mode}.userid = users.id"
-        f" WHERE scores_{mode}.id = %s", [sid])
-
-    s = f'{int(score["n100"]) + int(score["n300"])}p{score["n50"]}o{score["ngeki"]}o{score["nkatu"]}t{score["nmiss"]}a{score["map_md5"]}r{score["max_combo"]}e{bool(score["perfect"])}y{score["name"]}o{score["score"]}u{score["grade"]}{score["mods"]}True'
+    score = await db.fetch(f'SELECT scores_{mode}.*, users.name FROM scores_{mode} LEFT JOIN users ON scores_{mode}.userid = users.id WHERE scores_{mode}.id = {sid}')
+    if score["perfect"] == 1:
+        combo_bool = True
+    else:
+        combo_bool = False
+    s = f'{int(score["n100"]) + int(score["n300"])}p{score["n50"]}o{score["ngeki"]}o{score["nkatu"]}t{score["nmiss"]}a{score["map_md5"]}r{score["max_combo"]}e{combo_bool}y{score["name"]}o{score["score"]}u{score["grade"]}{score["mods"]}True'
     hashh = hashlib.md5()
     hashh.update(s.encode("utf-8"))
     nhash = hashh.hexdigest()
@@ -54,24 +51,15 @@ async def build_replay(sid: int, mode: str):
     ])
     return full
 
-@app.route("/<int:_id>")
-async def avatar(_id: int):
-    if os.path.isfile(f"{DIR}/{_id}.png"):
-        _file = _id
-    else:
-        _file = "default"
-    
-    return await send_file(f"{DIR}/{_file}.png")
-
-@app.route("/replay/<str:mode>/<int:_id>")
-async def dl_replay(_id: int, mode: str):
-    replay = await build_replay(_id, mode)
-    _file = Response(replay)
-    _file.headers["Content-type"] = "application/octet-stream"
-    _file.headers["Content-length"] = len(replay)
-    _file.headers["Content-Description"] = "File Transfer"
-    _file.headers["Content-Disposition"] = f"attachment; filename={_id}.osr"
-    return _file
+@app.route("/replay/<mode>/<int:id>")
+async def dl_replay(id, mode):
+    replay = await build_replay(id, mode)
+    file = Response(replay)
+    file.headers["Content-type"] = "application/octet-stream"
+    file.headers["Content-length"] = len(replay)
+    file.headers["Content-Description"] = "File Transfer"
+    file.headers["Content-Disposition"] = f"attachment; filename={id}.osr"
+    return file
 
 if __name__ == '__main__':
     app.run()
