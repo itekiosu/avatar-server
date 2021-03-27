bbeecd2b437909a0b14146724e246b3b9495ed94
James <51536154+tsunyoku@users.noreply.github.com>
2021-03-27 13:20:51 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,27 +1,23 @@
 import data as dataTypes
 import struct
 
-def uleb128Encode(num: int):
-	if not num:
-		return bytearray(b"\x00")
-
+def uleb128Encode(num):
 	arr = bytearray()
 	length = 0
-
+	if num == 0:
+		return bytearray(b"\x00")
 	while num > 0:
 		arr.append(num & 127)
 		num >>= 7
 		if num != 0:
 			arr[length] |= 128
-		length += 1
-
+		length+=1
 	return arr
 
 def pack_data(__data, __dataType):
 	data = bytes()
 	pack = True
 	packType = "<B"
-
 	if __dataType == dataTypes.bbytes:
 		pack = False
 		data = __data
@@ -33,36 +29,36 @@ def pack_data(__data, __dataType):
 			data += b"\x0B"
 			data += uleb128Encode(len(__data))
 			data += str.encode(__data, "latin_1")
+	elif __dataType == dataTypes.uInt16:
+		packType = "<H"
+	elif __dataType == dataTypes.sInt16:
+		packType = "<h"
+	elif __dataType == dataTypes.uInt32:
+		packType = "<L"
+	elif __dataType == dataTypes.sInt32:
+		packType = "<l"
+	elif __dataType == dataTypes.uInt64:
+		packType = "<Q"
+	elif __dataType == dataTypes.sInt64:
+		packType = "<q"
+	elif __dataType == dataTypes.string:
+		packType = "<s"
+	elif __dataType == dataTypes.ffloat:
+		packType = "<f"
 	elif __dataType == dataTypes.rawReplay:
 		pack = False
 		data += pack_data(len(__data), dataTypes.uInt32)
 		data += __data
-	else:
-		packType = {
-			dataTypes.uInt16: "<H",
-			dataTypes.sInt16: "<h",
-			dataTypes.uInt32: "<L",
-			dataTypes.sInt32: "<l",
-			dataTypes.uInt64: "<Q",
-			dataTypes.sInt64: "<q",
-			dataTypes.string: "<s",
-			dataTypes.ffloat: "<f"
-		}.get(__dataType)
-
 	if pack:
 		data += struct.pack(packType, __data)
-
 	return data
 
 def binary_write(structure = None):
-	if not structure:
+	if structure is None:
 		structure = []
-
 	packetData = bytes()
-
 	for i in structure:
 		packetData += pack_data(i[0], i[1])
-
 	return packetData
 
 def replay_time(unixTime):
